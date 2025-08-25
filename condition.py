f4ab746da3b8d83c0a05aa4637008f4d9860d78b
subham-dhawaliya <skgdhawaliya@gmail.com>
2025-08-25 09:48:32 +0530

    python  ondition

---
+++ condition.py
@@ -0,0 +1,11 @@
+ligth = input("ligth color :")
+
+if(ligth == "Red"):
+    print("stop")
+elif(ligth == "Yellow"):
+    print("look")
+elif(ligth == "Green"):
+    print("Go")
+else:
+    print("broken ligth")
+    
