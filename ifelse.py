446df012663ec629977b9460cf53fabcc3ab3323
subham-dhawaliya <skgdhawaliya@gmail.com>
2025-08-25 21:09:44 +0530

    2ndcondition

---
+++ ifelse.py
@@ -0,0 +1,9 @@
+marks=int(input("marks := "))
+if (marks >= 90):
+    print("A")
+elif(marks >= 80) and (marks < 90):
+    print("B")
+elif(marks >=70) and (marks <80):
+    print("D")
+else:
+    print("Fail")
