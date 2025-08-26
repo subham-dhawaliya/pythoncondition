f485de28475bd4d4ec115481ebcb3e1e121c0969
subham-dhawaliya <skgdhawaliya@gmail.com>
2025-08-26 22:13:40 +0530

    practice question

---
+++ practice.py
@@ -0,0 +1,23 @@
+#addition two number
+
+num1 = 23
+num2 = 34
+sum = num1 + num2
+print("sum =", sum)
+
+#addition two number taking by user
+num1 = int(input("enter first num :"))
+num2 = int(input("enter second num :"))
+sum = num1 + num2
+print("enter two number : ", sum)
+
+#square of area
+
+side1 = int(input("one square side : "))
+print("square of area",side1 **2)
+
+# Average of two vlues
+num1 = float(input("enter first value :"))
+num2 = float(input("enter sec valuen: "))
+print("average number of two value = ",(num1+num2)/2)
+
