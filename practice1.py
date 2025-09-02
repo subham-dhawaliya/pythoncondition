4135df8e4c99b07f3f3992730bceb2c1b7939745
subham-dhawaliya <skgdhawaliya@gmail.com>
2025-09-02 16:01:29 +0530

    update

---
+++ practice1.py
@@ -0,0 +1,73 @@
+# num = int(input("enter any number : "))
+
+# if num > 0:
+#     print("Positive")
+
+# elif num < 0:
+#     print("Negative")
+# else:
+#     print("zero")
+
+    # check if a number is even or odd
+    # 
+# num = int(input("Check Even or Odd number : "))
+
+# if num%2 == 0:
+#         print("Even")
+
+# else:
+#         print("Odd")
+
+# Largest of two number
+
+# num1 = int(input("enter first number : "))
+# num2 = int(input("enter second number : "))
+
+# if num1>num2:
+#         print("largest num1 = ",num1)
+# else:
+#         print("largest num2 = ",num2)
+
+#largest of three numbr
+
+# num1 = int(input("enter first number : "))
+# num2 = int(input("enter second number : "))
+# num3 = int(input("enter third number : "))
+
+
+# if num1>num2  and num1>num3:
+#         print("largest num1 = ",num1)
+# elif num2>num1 and num2>num3:
+#         print("largest num2 = ", num2)
+# else:
+        # print("largest num3 = ",num3)
+
+#leaf year
+
+year = int(input("find the leaf year : "))
+if(year%4 == 0 and year%100!=0) or (year%400 == 0):
+        print("leaf year : ")
+else:
+        print("not leaf year : ")
+
+#gradecian number
+# num = int(input("Enter a number :"))
+
+# if num>=90:
+#     print("grade = A")
+# elif num>=80:
+#     print("grade = B")
+# elif num>=70:
+#     print("grade = c")
+# else:
+#     print("grade = d ")
+
+#check if a find vowel number
+
+# a = input("enter any number : ") 
+
+# if a in 'aeiou':
+#         print("vowel number")
+# else:
+#         print("cosnant")
+        
